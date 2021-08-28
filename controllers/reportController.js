0d4d6dce270840e112530e25badd93b2c5b65ec4
misaeliana <69031743+misaeliana@users.noreply.github.com>
2021-08-28 13:33:53 +0800

    update sales report
    - count and totals show
    - filter does not work yet
    - still needs to be sorted by count

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -10,6 +10,10 @@ const Units = require('../models/UnitsModel.js');
 
 const Dishes = require('../models/DishesModel.js');
 
+const Sales = require('../models/SalesModel.js');
+
+const SalesDishes = require('../models/SalesDishesModel.js');
+
 //import models
 
 const reportController = {
@@ -63,6 +67,7 @@ const reportController = {
         var dishes = [];
         var total = 0;
         var discount = 0;
+        var VAT = 0;
 
         var projection = '_id dishName dishPrice dishClassification';
 
@@ -74,15 +79,71 @@ const reportController = {
                     dishName: result[i].dishName,
                     count: 0,
                     price: parseFloat(result[i].dishPrice).toFixed(2),
-                    total: parseFloat(0).toFixed(2)
+                    total: 0
                 };
+
+                //dish.total = parseFloat(dish.count * dish.price).toFixed(2);
+                //total += dish.total;
                 dishes.push(dish);
             }
 
-            discount = parseFloat(discount).toFixed(2);
-            total = parseFloat(total).toFixed(2);
-            
-            res.render('viewSalesReport', {today, dishes, total, discount});
+            // look through sales 
+            var projection2 = '_id date total VAT discount';
+            db.findMany(Sales, {}, projection2, function(result2) {
+                var salesIDs = [];
+
+                for (var j = 0; j < result2.length; j++) {
+                    VAT += result2[j].VAT;
+                    discount += result2[j].discount;
+                    total += result2[j].total;
+
+                    salesIDs.push(result2[j]._id);
+                }
+
+                //console.log(salesIDs);
+                //console.log(result2);
+                VAT = parseFloat(VAT).toFixed(2);
+                discount = parseFloat(discount).toFixed(2);
+                total = parseFloat(total).toFixed(2);
+
+                function getSalesDish (saleID) {
+                    return new Promise ((resolve, reject) => {
+                        db.findMany (SalesDishes, {salesID: saleID}, 'salesID dishID quantity', function(result) {
+                            if (result!="")
+                                resolve(result);
+                        });
+                    });
+                }
+
+                async function getCount(today, dishes, total, discount, VAT, salesIDs) {
+                    for (var k = 0; k < salesIDs.length; k++) {
+                        //console.log(salesIDs[k]);
+
+                        // get salesID dishID quantity
+                        var salesDish = await getSalesDish(salesIDs[k]);
+
+                        for (var l = 0; l < salesDish.length; l++) {
+                            var dishID = salesDish[l].dishID;
+                            var quantity = salesDish[l].quantity;
+                            //console.log(salesDish[l]);
+
+                            for (var m = 0; m < dishes.length; m++) {
+                                if (dishID == dishes[m]._id) {
+                                    dishes[m].count += quantity;
+                                }
+                            }
+                        }
+                    }
+
+                    for (var n = 0; n < dishes.length; n++) 
+                        dishes[n].total = parseFloat(dishes[n].count * dishes[n].price).toFixed(2);
+
+                    res.render('viewSalesReport', {today, dishes, total, discount, VAT});
+                }
+
+                getCount(today, dishes, total, discount, VAT, salesIDs);
+                
+            });
         });
     }
 };
